89ded6624f624f9e5edef11d45c3b96396d91dda
Ownclo - Arcis <mrownclo@gmail.com>
2013-04-19 20:32:08 +0400

    Add is now a macrocommand

--- use.sh
+++ use.sh
@@ -1,6 +1,17 @@
+# CREATING NEW LAB
 zenlabs new box
 
 cd box
 
+# GENERATING TABLE
 zenlabs process inp.dat rule1.py
-zenlabs add inp_rule1.dat
+zenlabs add table inp_rule1.dat
+
+# GENERATING PLOTS
+#zenlabs generate plot SOURCE_TABLE_NAME [FIELDS]
+#zenlabs add plot [SECTION=plots]
+
+## NOTE ##
+# there are 'add table' and 'add plot',
+# so we need a little bit of a structure in our ZenLabs
+# script.
